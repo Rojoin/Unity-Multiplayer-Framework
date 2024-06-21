f4b9cc581369b2000a328be4cd12e502cee76b6a
Rojoin <nachoarrastua@gmail.com>
2024-06-21 05:30:30 -0300

    Add NetObjectFactory

---
+++ RojoinNetworkSystem/NetObjectFactory.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections.Generic;
+
+namespace RojoinNetworkSystem
+{
+    public static class NetObjectFactory
+    {
+        public static int intanceCount = 0;
+
+        public static int getNewIntances() => intanceCount++;
+    }
+}
