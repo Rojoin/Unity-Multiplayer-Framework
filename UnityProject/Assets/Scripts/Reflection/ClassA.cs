4b09098de8f829e245ccbe037d1c905f928e1e6a
Rojoin <nachoarrastua@gmail.com>
2024-06-21 18:50:15 -0300

    Add Test

--- UnityProject/Assets/Scripts/Reflection/ClassA.cs
+++ UnityProject/Assets/Scripts/Reflection/ClassA.cs
@@ -2,6 +2,7 @@ using System;
 using System.Collections;
 using System.Collections.Generic;
 using System.Reflection;
+using RojoinNetworkSystem;
 using UnityEngine;
 
 public class ClassA
@@ -17,35 +18,7 @@ public class ClassA
     }
 }
 
-public class NetValue : Attribute 
-{
-    int id;
-    public NetValue(int id) 
-    {
-        this.id = id;
-    }
-}
 
-public static class FieldInfoExtensions 
-{
-    public static List<FieldInfo> GetFields(this Vector3 vector3) 
-    {
-        List<FieldInfo> output = new List<FieldInfo>();
-        output.Add(vector3.GetType().GetField("x"));
-        output.Add(vector3.GetType().GetField("y"));
-        output.Add(vector3.GetType().GetField("z"));
-        return output;
-    }
 
-    public static List<FieldInfo> GetFields(this Quaternion quaternion)
-    {
-        List<FieldInfo> output = new List<FieldInfo>();
-        output.Add(quaternion.GetType().GetField("x"));
-        output.Add(quaternion.GetType().GetField("y"));
-        output.Add(quaternion.GetType().GetField("z"));
-        output.Add(quaternion.GetType().GetField("w"));
-        return output;
-    }
-}
 
 
